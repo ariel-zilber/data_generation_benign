e6ef694d9f65eaae985c0082019d2d1c42f7d07e
ariel <ariel.zilbershtey@toganetworks.com>
2021-05-30 13:57:19 +0300

    first commit

---
+++ generate_stack_simple.py
@@ -0,0 +1,53 @@
+""" This module is used to create simple installation scripts
+
+"""
+
+LIB_TEMPLATE = """function easy_install_%s {
+	echo "%s"
+	sleep 3
+	%s
+	echo "%s"
+	sleep 3
+}"""
+
+LIB_RELATIVE_PATH = "shellstack/lib/"
+
+RECIPES_TEMPLATE = """#!/bin/bash
+
+source "$LIB_PATH/%s.sh"
+function defaults_basic {
+	#TODO: verify if vars are set and then use defautls our get 'em another way
+	HOST_NAME="myhost"
+	USER_NAME="myuser"
+	USER_PASSWORD="mys3cr3t"
+	USER_SSH_KEY="ssh-rsa paste here your ~/.ssh/id_rsa.pub"
+}
+
+function install_%s {
+	install_basic
+   easy_install_%s
+}"""
+
+RECIPES_RELATIVE_PATH = "shellstack/recipes/"
+
+
+def create_lib_file(package_name: str, install_command: str):
+    """
+
+    :param package_name:
+    :param install_command:
+    :return:
+    """
+    with open(LIB_RELATIVE_PATH + "/" + package_name + ".sh", "w") as f:
+        f.write(LIB_TEMPLATE % (package_name, "Will now install " + package_name,
+                                install_command, package_name + " has been installed")
+                )
+
+
+def create_recipe(recipe_name):
+    with open(RECIPES_RELATIVE_PATH + "/" + recipe_name, 'w') as f:
+        f.write(RECIPES_TEMPLATE % (recipe_name,recipe_name, recipe_name))
+
+
+create_lib_file("python3_9", "sudo apt install python3.9")
+create_recipe("python3_9")
---
+++ shellstack/lib/python3_9.sh
@@ -0,0 +1,7 @@
+function easy_install_python3_9 {
+	echo "Will now install python3_9"
+	sleep 3
+	sudo apt install python3.9
+	echo "python3_9 has been installed"
+	sleep 3
+}
