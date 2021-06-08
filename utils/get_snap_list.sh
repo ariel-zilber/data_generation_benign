963b8bc44cea4abecaac023ceef3afea47f6b1c3
ariel <ariel.zilbershtey@toganetworks.com>
2021-06-08 14:47:07 +0300

    S

--- utils/get_snap_list.sh
+++ utils/get_snap_list.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
-
-# reset file
-echo "" >snap-list.txt
-
-# search packages
-for x in {{a..z},{A..Z},{1..9}}
-do
-  echo "Letter: ${x}"
-  snap find "${x}" | awk '{if ($1!="Name") print $1}' >> snap-list.txt
-done
-
-# Declare a string array with type
-declare -a StringArray=("Windows" "intel" "python" "java"  "go" "rust" "nginx" "game" "PHP"
-"C#"  "C++"  "VB.Net"  "Python" "Perl" )
-
-# Read the array values with space
-for x in "${StringArray[@]}"; do
-   snap find "${x}" | awk '{if ($1!="Name") print $1}' >> snap-list.txt
-done
-
-
-# remove repeating
-cat snap-list.txt  | sort| uniq > snap-list-uniq.txt
-rm snap-list.txt
-mv snap-list-uniq.txt snap-list.txt
-
-
-
-# get install python command
-cat snap-list.txt | awk '{print "install_basic(\""$1"\", \"sudo snap install "$1"\")"}' > command.txt
-rm snap-list.txt
-mv command.txt snap-list.txt
-
+#
+## reset file
+#echo "" >snap-list.txt
+#
+## search packages
+#for x in {{a..z},{A..Z},{1..9}}
+#do
+#  echo "Letter: ${x}"
+#  snap find "${x}" | awk '{if ($1!="Name") print $1}' >> snap-list.txt
+#done
+#
+#
+## remove repeating
+#cat snap-list.txt  | sort| uniq > snap-list-uniq.txt
+#rm snap-list.txt
+#mv snap-list-uniq.txt snap-list.txt
+#
+#
+#
+## get install python command
+##cat snap-list.txt | awk '{print "install_basic(\""$1"\", \"sudo snap install "$1"\")"}' > command.txt
+##rm snap-list.txt
+##mv command.txt snap-list.txt
+#
